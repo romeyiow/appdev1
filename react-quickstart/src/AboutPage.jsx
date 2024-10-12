ca74a836f4b811f1fb6903725ae759ca9e3e4bf6
romeyiow <jimperialyosh@gmail.com>
2024-10-12 16:12:45 +0800

    I’m done completing the section on writing markup with JSX

---
+++ react-quickstart/src/AboutPage.jsx
@@ -0,0 +1,9 @@
+export default function AboutPage() {
+    //Section 2: Writing markup with JSX}
+    return (
+        <>
+            <h1>About</h1>
+            <p>Hello there.<br />How do you do?</p>
+        </>
+    );
+}
--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -3,27 +3,11 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-//Section 9: Using hooks -> Sharing Data Between Components
-
-function MyButton({ count, onClick }) {
-  return (
-    <button onClick={onClick}>
-      Clicked {count} times
-    </button>
-  );
-}
+//Section 2: Writing markup with JSX}
 
 export default function App() {
-  const [count, setCount] = useState(0);
-  function handleClick() {
-    setCount(count + 1);
-  }
   return (
     <>
-      <h1>Counters that update together</h1>
-      <MyButton count={count} onClick={handleClick} />
-      <MyButton count={count} onClick={handleClick} />
-      <MyButton count={count} onClick={handleClick} />
     </>
   );
 }
