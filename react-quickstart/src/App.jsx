2deac5fbf36fe1e4912bc3739b6d89e09317c9f9
romeyiow <jimperialyosh@gmail.com>
2024-10-12 16:34:32 +0800

    I’m done completing the section on displaying data

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -3,15 +3,10 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-//Section 3: Adding styles
-
-export default function App() {
+export default function Profile() {
   return (
     <>
-      <img className="avatar" src='https://i.imgur.com/yXOvdOSs.jpg'/>
+     
     </>
   );
 }
-
-
-
---
+++ react-quickstart/src/Profile.jsx
@@ -0,0 +1,26 @@
+
+//Section 3: Adding styles
+const user = {
+    name: 'Hedy Lamarr',
+    imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+    imageSize: 90,
+};
+
+export default function Profile() {
+    return (
+        <>
+            <img
+                className="avatar"
+                src={user.imageUrl}
+                alt={'Photo of ' + user.name}
+                style={{
+                    width: user.imageSize,
+                    height: user.imageSize,
+                    border: "2px solid #111",
+                    borderRadius:"50%"
+                }}
+            />
+            <h3>{user.name}</h3>
+        </>
+    );
+}
