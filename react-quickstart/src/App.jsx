fd8a02909683cc8991b12c8074b04d11f79f52a0
romeyiow <jimperialyosh@gmail.com>
2024-10-12 12:09:59 +0800

    I’m done completing the section on creating and nesting components.

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -3,12 +3,21 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+//Section 1: Creating and nesting components 
+
+function MyButton() {
+  return (
+    <button>I'm a button</button>
+  );
+}
+
+export default function App() {
   return (
     <>
-      
+      <h1>Welcome to my app</h1>F
+      <MyButton />
     </>
   )
 }
 
-export default App
+
