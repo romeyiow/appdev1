acd07c68c603f0e65f57ffc0ba5bed4940782855
romeyiow <jimperialyosh@gmail.com>
2024-10-12 14:11:33 +0800

    I’m done completing the section on updating the screen

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -3,17 +3,34 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-//Section 1:  Creating and nesting components
+//Section 8: Updating the screen a.k.a state management
 
 function MyButton() {
+  const [count, setCount] = useState(10);
+  const [task, setTask] = useState(0);
+  const [comments, setComments] = useState(0);
+  const [state, setState] = useState("Off");
+  function handleClick() {
+    state == "On" ? setState("Off") : setState("On");
+  }
   return (
-    <button>I'm a button</button>
+    <>
+      <button onClick={handleClick} >
+        {/* Clicked {count} times */}
+        {state}
+      </button>
+    </>
   );
 }
+
+
 export default function App() {
   return (
     <>
-      <h1>Welcome to my app</h1>
+      <h3>Click the switches below</h3>
+      <MyButton />
+      <h6>shows separately rendered instance of the MyButton component to show how each button “remembers” its own count state and doesn’t affect
+        other buttons</h6>
       <MyButton />
     </>
   )
