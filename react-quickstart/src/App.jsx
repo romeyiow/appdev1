e43467b3980974390a4bd4f71fd53c25928c601e
romeyiow <jimperialyosh@gmail.com>
2024-10-12 12:41:05 +0800

    I’m done completing the section on rendering lists

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -3,19 +3,28 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-//Section 1: Creating and nesting components 
-
-function MyButton() {
-  return (
-    <button>I'm a button</button>
-  );
-}
-
+//Section 6: Rendering lists 
 export default function App() {
+  const products = [
+    { title: 'Cabbage', isFruit: false, id: 1 },
+    { title: 'Garlic', isFruit: false, id: 2 },
+    { title: 'Apple', isFruit: true, id: 3 },
+  ];
+  const listItems = products.map(product => //.map() iterates listItems elements 
+    <li
+      key={product.id}
+      style={{
+        color: product.isFruit ? 'magenta' : 'darkgreen'
+      }}
+    >
+      {product.title}
+
+    </li>
+  );
   return (
     <>
-      <h1>Welcome to my app</h1>F
-      <MyButton />
+      <h1>List of Products</h1>
+      <ul>{listItems}</ul>
     </>
   )
 }
